5323ee59cfebbde191de7d0b81eaa6fa0b6b5bc8
nhà thơ tình tuyệt vọng <khatmausr502@gmail.com>
2020-11-13 14:14:31 +0700

    fix bugs

--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
+#include <cmath>
+#include <cstring>
 #include <fstream>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <stack>
 #include <string>
 #include <vector>
-#include <cmath>
+
 using namespace std;
 
+/// function isNum
+/// Check whether input string is a number or not.
+/// Return true if input string is a number or false, vice versa.
 bool isNum(string num) {
-    if (num.back() == '.' || num.front() == '.') return false;
+    if (num.back() == '.' || num.front() == '.') return false;  // Check whether there is dot at the front/back of the string
     bool dotted = false;
     for (int i = 0; i < num.length(); ++i) {
-        if ((!isdigit(num[i]) && !(num[i] == '.')) ||
-            (num[i] == '.' && dotted))
+        if ((!isdigit(num[i]) && !(num[i] == '.')) ||  // Whether a char is not digit or a dot(.)
+            (num[i] == '.' && dotted))                 // There are 2 dots in the string
             return false;
-        if (num[i] == '.') {
+        if (num[i] == '.') {  // Input number with number of decimal digit > 2
             if (i < num.length() - 1 - 2)
                 return false;
             dotted = true;
@@ -23,35 +28,48 @@ bool isNum(string num) {
     return true;
 }
 
+/// function isOperator
+/// Check whether input char is an operator
+/// return true if input char is an operator or false, vice versa
 bool isOperator(char o) {
     return (o == '^' || o == '*' || o == '/' || o == '+' || o == '-');
 }
 
+/// function isBracket
+/// Check whether input char is an bracket
+/// return true if input char is an bracket or false, vice versa
 bool isBracket(char o) {
     return (o == '(' || o == ')' || o == '[' || o == ']' || o == '{' || o == '}');
 }
 
-vector<string> readExp(string input_file, unsigned int limit) {
-    ifstream fin(input_file);
-    vector<string> exp;
-    unsigned int linesCounter = 0;
-    while (linesCounter < limit && !fin.eof()) {
-        string line;
-        getline(fin, line, '\n');
-        exp.push_back(line);
+/// function isStandardizedExpression
+/// Check whether input expression is valid
+/// return true if input expression is valid or false, vice versa
+void isStandardizedExpression(string s) {
+    if (s.front() == ' ' || s.back() == ' ') throw "Invalid expression: front | back";  // Check if
+    for (int i = 1; i < s.length() - 1; ++i) {
+        if (isOperator(s[i]) && (s[i - 1] != ' ' || s[i + 1] != ' ')) throw "Invalid expression: spaces around operator";
+        if (s[i] == ' ' && s[i + 1] == ' ') throw "Invalid expression: spaces";
     }
-    fin.close();
-    return exp;
 }
 
-void standardizedExpression(string* s) {
-    while (s->at(0) == ' ') s->erase(0, 1);
-    while (s->at(s->length() - 1) == ' ') s->erase(s->length() - 1, 1);
-    for (int i = 0; i < s->length() - 1; ++i) {
-        while (s->at(i) == ' ' && s->at(i + 1) == ' ') s->erase(i, 1);
-        while (s->at(i) == '(' && s->at(i + 1) == ' ') s->erase(i + 1, 1);
-        while (s->at(i) == ' ' && s->at(i + 1) == ')') s->erase(i, 1);
+unsigned short bracketPrecedence(char o) {
+    if (!isBracket(o)) return 0;
+    switch (o) {
+        case '{':
+        case '}':
+            return 3;
+            break;
+        case '[':
+        case ']':
+            return 2;
+            break;
+        case '(':
+        case ')':
+            return 1;
+            break;
     }
+    return 0;
 }
 
 unsigned short precedence(char o) {
@@ -111,16 +129,16 @@ double calculate(double val1, double val2, char op) {
     return 0;
 }
 
-//32.34
 double calcInfix(string exp) {
-    char lastOp = 0;
+    bool lastOp = false;
     stack<double> operandStack;
+    stack<char> bracketStack;
     stack<char> operatorStack;
     while (!exp.empty()) {
         if (isblank(exp[0])) exp.erase(0, 1);
         if (isdigit(exp[0])) {
-            if (lastOp == 1) throw "Infix invalid: Double number catched!";
-            lastOp = 1;
+            if (lastOp) throw "Infix invalid: Double number catched!";
+            lastOp = true;
             int delim = 0;
             while (isdigit(exp[delim]) || exp[delim] == '.') ++delim;
             double operand = convertNumber(exp.substr(0, delim));
@@ -129,6 +147,9 @@ double calcInfix(string exp) {
         }
         if (exp[0] == '(' || exp[0] == '[' || exp[0] == '{') {
             operatorStack.push(exp[0]);
+            if (!bracketStack.empty() && bracketPrecedence(exp[0]) >= bracketPrecedence(bracketStack.top()))
+                throw "Invalid bracket order!";
+            bracketStack.push(exp[0]);
             exp.erase(0, 1);
         }
         if (exp[0] == ')' || exp[0] == ']' || exp[0] == '}') {
@@ -144,14 +165,15 @@ double calcInfix(string exp) {
                 else
                     operandStack.push(calculate(val1, val2, op));
             }
-            char bracket = operatorStack.top();
+            char bracket = bracketStack.top();
             operatorStack.pop();
-            if (!isPairBracket(bracket, exp[0])) throw "Bracket order invalid!";
+            bracketStack.pop();
+            if (!isPairBracket(bracket, exp[0])) throw "Bracket pair invalid!";
             exp.erase(0, 1);
         }
         if (isOperator(exp[0])) {
-            if (lastOp == -1) throw "Infix invalid: Double operator catched!";
-            lastOp = -1;
+            if (!lastOp) throw "Infix invalid: Double operator catched!";
+            lastOp = false;
             while (!operatorStack.empty() &&
                    precedence(operatorStack.top()) >= precedence(exp[0])) {
                 double val2 = operandStack.top();
@@ -185,14 +207,15 @@ double calcInfix(string exp) {
 }
 
 string infix2Postfix(string exp) {
-    char lastOp = 0;
+    bool lastOp = false;
     string ans = "";
+    stack<char> bracketStack;
     stack<char> operatorStack;
     while (!exp.empty()) {
         if (isblank(exp[0])) exp.erase(0, 1);
         if (isdigit(exp[0])) {
-            if (lastOp == 1) throw "Infix invalid: Double number catched!";
-            lastOp = 1;
+            if (lastOp) throw "Infix invalid: Double number catched!";
+            lastOp = true;
             int delim = 0;
             while (isdigit(exp[delim]) || exp[delim] == '.') ++delim;
             string num = exp.substr(0, delim);
@@ -204,6 +227,9 @@ string infix2Postfix(string exp) {
         }
         if (exp[0] == '(' || exp[0] == '[' || exp[0] == '{') {
             operatorStack.push(exp[0]);
+            if (!bracketStack.empty() && bracketPrecedence(exp[0]) >= bracketPrecedence(bracketStack.top()))
+                throw "Invalid bracket order!";
+            bracketStack.push(exp[0]);
             exp.erase(0, 1);
         }
         if (exp[0] == ')' || exp[0] == ']' || exp[0] == '}') {
@@ -215,16 +241,17 @@ string infix2Postfix(string exp) {
                 else
                     ans = ans + op + " ";
             }
-            char bracket = operatorStack.top();
+            char bracket = bracketStack.top();
             operatorStack.pop();
+            bracketStack.pop();
             if (!isPairBracket(bracket, exp[0])) throw "Bracket order invalid!";
             exp.erase(0, 1);
         }
         if (isOperator(exp[0])) {
-            if (lastOp == -1) throw "Infix invalid: Double operator catched!";
-            lastOp = -1;
+            if (!lastOp) throw "Infix invalid: Double operator catched!";
+            lastOp = false;
             while (!operatorStack.empty() &&
-                   precedence(operatorStack.top()) > precedence(exp[0])) {
+                   precedence(operatorStack.top()) >= precedence(exp[0])) {
                 char op = operatorStack.top();
                 operatorStack.pop();
                 if (!isOperator(op))
@@ -246,17 +273,42 @@ string infix2Postfix(string exp) {
     }
     return ans;
 }
-int main() {
-    string exp = "15 ^ (20 / (5 * 20) + 10";
-    //getline(fin, exp, '\n');
+void test(string exp){
     try {
-        standardizedExpression(&exp);
-        // double ans = calcInfix(exp);
-        string ans = infix2Postfix(exp);
-        cout << ans;
+        isStandardizedExpression(exp);
+        double ans = calcInfix(exp);
+        cout << setprecision(2) << ans << endl;
     } catch (char const* e) {
-        cout << e;
+        cout << "E\n";
     }
+}
 
+void exec(char* argv[]){
+    ifstream fin(argv[1]);
+    ofstream fout(argv[4]);
+    int n = atoi(argv[2]);
+    for (int i = 0; i < n; ++i) {
+        string exp;
+        if (getline(fin, exp, '\n')) {
+            try {
+                isStandardizedExpression(exp);
+                if (!strcmp(argv[3], "-c")) {
+                    double ans = calcInfix(exp);
+                    fout << setprecision(2) << ans << endl;
+                } else if (!strcmp(argv[3], "-t")) {
+                    string ans = infix2Postfix(exp);
+                    fout << ans << endl;
+                }
+            } catch (char const* e) {
+                fout << "E\n";
+            }
+        }
+    }
+    fin.close();
+    fout.close();
+}
+int main(int argc, char* argv[]) {
+    //test("(5 + 5}");
+    exec(argv);
     return 0;
 }
